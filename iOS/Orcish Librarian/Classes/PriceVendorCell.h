8117a417b7c5ab96d70abd973010a2c2810afa8d
Stewart Ulm <stewart.ulm@gmail.com>
2012-04-30 23:15:51 -0400

    Scroll card view to top after going to Gatherer page

---
+++ iOS/Orcish Librarian/Classes/PriceVendorCell.h
@@ -0,0 +1,13 @@
+//
+//  PriceVendorCell.h
+//  Orcish Librarian
+//
+//  Created by Stewart Ulm on 4/30/12.
+//  Copyright (c) 2012 Orcish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PriceVendorCell : UITableViewCell
+
+@end
---
+++ iOS/Orcish Librarian/Classes/PriceVendorCell.m
@@ -0,0 +1,29 @@
+//
+//  PriceVendorCell.m
+//  Orcish Librarian
+//
+//  Created by Stewart Ulm on 4/30/12.
+//  Copyright (c) 2012 Orcish. All rights reserved.
+//
+
+#import "PriceVendorCell.h"
+
+@implementation PriceVendorCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
