189cc0984dcc8c8e8c175db4f79d08699c5920f6
Stewart Ulm <stewart.ulm@gmail.com>
2012-06-18 00:04:30 -0400

    Checkpoint on Advanced Search feature (Incomplete)

---
+++ iOS/Orcish Librarian/Classes/Controllers/Advanced/FacetOptionOracleTextController.h
@@ -0,0 +1,13 @@
+//
+//  FacetOptionTextController.h
+//  Orcish Librarian
+//
+//  Created by Stewart Ulm on 6/17/12.
+//  Copyright (c) 2012 Orcish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacetOptionTextController : UIViewController
+
+@end
---
+++ iOS/Orcish Librarian/Classes/Controllers/Advanced/FacetOptionOracleTextController.m
@@ -0,0 +1,43 @@
+//
+//  FacetOptionTextController.m
+//  Orcish Librarian
+//
+//  Created by Stewart Ulm on 6/17/12.
+//  Copyright (c) 2012 Orcish. All rights reserved.
+//
+
+#import "FacetOptionTextController.h"
+
+@interface FacetOptionTextController ()
+
+@end
+
+@implementation FacetOptionTextController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
